e501c6c27442216dcc2d9e49c0d7340e5f791610
FADEwt <diamonvhero@gmail.com>
2023-08-18 15:25:59 +0100

    Added the user and message models

--- models/message.js
+++ models/message.js
@@ -3,15 +3,14 @@ const Schema = mongoose.Schema;
 
 
 const MessageSchema = new Schema({
-    id: { type: mongoose.Types.ObjectId, auto: true },
-    user_id: { type: String, required: true },
-    message: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    message: { type: String, required: true }
 });
   
 // Virtual for book's URL
 MessageSchema.virtual("url").get(function () {
 // We don't use an arrow function as we'll need the this object
-    return `/message/${this._id}`;
+    // return `${this.user}`;
 });
 
 // Export model
--- models/user.js
+++ models/user.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const UserSchema = new Schema({
-    id: { type: mongoose.Types.ObjectId, auto: true },
-    name: { type: String, required: true },
-    mail: { type: String, required: true },
-    password: { type: String, required: true },
+    name: { type: String, required: true, maxLength: 20 },
+    email: { type: String, lowercase: true, required: true },
+    member: { type: Boolean, default: false },
+    admin: { type: Boolean, default: false },
+    // password: { type: String, required: true },
 });
   
-// Virtual for book's URL
-UserSchema.virtual("url").get(function () {
+
+UserSchema.virtual("userLogin").get(function () {
 // We don't use an arrow function as we'll need the this object
-    return `/in/${this._id}`;
+    // return `${this.name} <${this.email}>`;
 });
 
 // Export model
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("user", UserSchema);
